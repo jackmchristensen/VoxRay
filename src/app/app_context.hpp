2450ed064329e1b0df8404f5c640518d0d49b1e2
Jack <jack@jackmchristensen.com>
2025-11-01 15:04:55 -0500

    Add list of cameras to AppContext struct
    This is a simple implementation that will allow for multiple cameras in a scene

--- src/app/app_context.hpp
+++ src/app/app_context.hpp
@@ -3,21 +3,31 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_video.h>
 
+#include <cstddef>
 #include <memory>
 #include <type_traits>
+#include <vector>
 
+#include "camera.hpp"
 #include "update_flags.hpp"
 
 using Win = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
 using GLc = std::unique_ptr<std::remove_pointer_t<SDL_GLContext>, decltype(&SDL_GL_DestroyContext)>;
 
+// Contains variables that need to exist for lifetime of entire app run
 struct AppContext {
+  // --- Window variables ---
   Win window{ nullptr, SDL_DestroyWindow };
   GLc gl_context{nullptr, SDL_GL_DestroyContext};
   int width{};
   int height{};
+
+  // --- Cameras ---
+  std::vector<cam::Camera>  cameras;
+  std::size_t               active = 0;
 };
 
+// Initial configuration settings
 struct AppConfig {
   const char* title = "OpenGL Window";
   int width = 1280;
