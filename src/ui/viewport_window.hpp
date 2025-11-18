d70ffae5063493baba88a10a4f0cd295b7400a91
Jack Christensen <jack@jackmchristensen.com>
2025-11-18 01:38:58 -0600

    Creates and manages the viewport window
    Separate from the other ImGui windows because it needs a framebuffer and
    texture in order to render.

---
+++ src/ui/viewport_window.hpp
@@ -0,0 +1,21 @@
+// ui/viewport_window.hpp
+#pragma once
+#include <string>
+
+#include "graphics/gl_utils.hpp"
+
+namespace ui {
+
+  struct ViewportWindow {
+    std::string name;
+    bool open = true;
+
+    graphics::Framebuffer fbo;
+    graphics::Texture texture;
+
+    int width = 800;
+    int height = 600;
+    size_t camera_index = 0;
+  };
+
+} // namespace ui
